fe9aa35487c67f456f31556c98203b45178d16c5
mgeekim <64mingn@gmail.com>
2024-08-22 23:59:02 +0900

    [2-1]

--- app/front/App.tsx
+++ app/front/App.tsx
@@ -22,21 +22,37 @@ import {
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View style={styles.container}></View>
-      <Text>텍스트</Text>
-      <Button title="버튼 이름" onPress={() => console.log("클릭됨!")}></Button>
-      <TextInput />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>텍스트</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>텍스트</Text>
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
+    flex:1,
+    backgroundColor: 'yellow',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer: {
+    flex:1,
     backgroundColor: 'red',
-    // margin: '10%',
-    marginHorizontal: 10,
-    marginVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   }
 });
 
