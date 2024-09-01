c0d9758e8cc74294fcd480e71a72a87f1b8d45c9
mark2-kim <mark2.kim@samsung.com>
2024-09-01 21:51:45 +0900

    [2-6]

--- app/front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ app/front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,5 +1,7 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import MapHomeScreen from '../../screens/MapHomeScreen';
+import FeedHomeScreen from '../../screens/FeedHomeScreen';
+import CalendarHomeScreen from '../../screens/CalendarHomeScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -7,6 +9,8 @@ const MainDrawerNavigator = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="MapHome" component={MapHomeScreen} />
+      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
+      <Drawer.Screen name="CalendarHome" component={CalendarHomeScreen} />
     </Drawer.Navigator>
   );
 };
