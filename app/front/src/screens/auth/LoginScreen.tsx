64b7925c967b7fe8e671acc298c125831510f1ac
mark2-kim <mark2.kim@samsung.com>
2024-09-05 00:23:45 +0900

    [3-2] Text box를 눌렀을 때 키보드를 숫자 혹은 이메일 입력 모드로 변경하도록 수정

--- app/front/src/screens/auth/LoginScreen.tsx
+++ app/front/src/screens/auth/LoginScreen.tsx
@@ -6,8 +6,17 @@ function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <InputFiled placeholder="이메일" error={'이메일을 입력하세요.'} />
-        <InputFiled placeholder="비밀번호" error={'비밀번호를 입력하세요'} />
+        <InputFiled
+          placeholder="이메일"
+          error={'이메일을 입력하세요.'}
+          inputMode="email"
+        />
+        <InputFiled
+          placeholder="비밀번호"
+          error={'비밀번호를 입력하세요'}
+          inputMode="numeric"
+          secureTextEntry
+        />
       </View>
     </SafeAreaView>
   );
